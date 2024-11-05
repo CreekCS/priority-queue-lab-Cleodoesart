fb25cc31d739be246e1c7348532c15d70b11c079
Cleo <blakebrun731@gmail.com>
2024-11-05 20:34:35 +0000

    i did the thing and Cole said it was good

--- PriorityQueueLab.java
+++ PriorityQueueLab.java
@@ -1,6 +1,29 @@
 // import statements
-
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.util.PriorityQueue;
+import java.io.FileNotFoundException;
 public class PriorityQueueLab {
+    public static void main(String[] args) {
+
+        try {
+            File file = new File("RandIntegers.txt");
+            Scanner scanner = new Scanner(file);
+            PriorityQueue queue = new PriorityQueue<Integer>();
+            while (scanner.hasNextLine()) {
+                queue.add(Integer.parseInt(scanner.nextLine()));
+            }
+            int size = queue.size();
+            for(int i = 0; i < size;i++){
+                System.out.println(queue.remove());
+            }
+
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        }
+    }
   
 }
 
